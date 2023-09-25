6f7aa62bf7e300a3c7cd12a4167cef58dd2deb01
Svjetlana <svjetlanatodoric03@gmail.com>
2023-09-25 11:50:47 +0200

    Navbar component

---
+++ pages/nav.php
@@ -0,0 +1,15 @@
+<div class="white-bg">
+    <div class="header-logo-container">
+        <img src="../img/Tergo_logo.png" alt="">
+    </div>
+    <div class="links">
+        <a href="index.php">HOME</a>
+        <a href="index.php#services">SERVICES</a>
+        <a href="index.php#about-us">ABOUT US</a>
+        <a href="#contact-us">KONTAKT</a>
+        <!-- <div class="search-icon">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div> -->
+    </div>
+
+</div>
