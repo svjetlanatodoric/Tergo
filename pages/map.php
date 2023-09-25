953e6d226882c3393e1343b10ace6cfdf49b55ac
Svjetlana <svjetlanatodoric03@gmail.com>
2023-09-25 11:51:03 +0200

    Map component

---
+++ pages/map.php
@@ -0,0 +1,3 @@
+<div>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1yaXKj95ONXU7qIZ3KVYN-crSGS_bpD4&ehbc=2E312F&noprof=1&z=14" width="100%" height="600px" style="border:0;" allowfullscreen=""></iframe>
+    </div>
