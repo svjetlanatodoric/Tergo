c5becb9180bcd6d1f57d764406b597e3f8f58e51
Svjetlana <svjetlanatodoric03@gmail.com>
2023-10-02 13:27:11 +0200

    Title changed from Tergo to warehousing

--- pages/warehousing.php
+++ pages/warehousing.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tergo</title>
+    <title>Warehousing services</title>
     <link rel="stylesheet" href="../css/banner.css">
     <link rel="stylesheet" href="../css/form.css">
     <link rel="stylesheet" href="../css/warehouse.css">
