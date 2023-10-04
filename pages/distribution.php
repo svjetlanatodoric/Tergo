cfb95d22e18c109694c38b1afab53159ffd6a61e
Svjetlana <svjetlanatodoric03@gmail.com>
2023-10-04 10:07:41 +0200

    Page with data about distribution services. Added square-to-circle animation on the bottom of the page.

---
+++ pages/distribution.php
@@ -0,0 +1,94 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Distribution services</title>
+    <link rel="shortcut icon" href="../img/o.png">
+
+    <link rel="stylesheet" href="../css/banner.css">
+    <link rel="stylesheet" href="../css/form.css">
+    <link rel="stylesheet" href="../css/distribution.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+    </script>
+</head>
+
+<body>
+
+    <?php
+    include("nav.php");
+
+    ?>
+
+    <div class="distribution-hero">
+
+        <div class="distribution-illustration">
+            <div class="distribution-overlay"></div>
+            <!-- <img src="../img/distribution-img.jpg" alt=""> -->
+
+            <div class="hero-section-text">
+                <p class="distribution-hero-text"> Reliable and affordable</p>
+                <p class="solution">distribution</p>
+                <p class="distribution-hero-text">of goods</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <!-- HEADING -->
+         <div class="distribution-heading">
+            <h2>FTL AND LTL FREIGHT SHIPPING</h2>
+        </div>
+        
+        <!-- CARDS -->
+        <div class="distribution-wrapper">
+     
+            <div class="distribution-service-wrapper">
+                <div class="card-icon">
+                <i class="fa-solid fa-truck-moving"></i>
+                            </div>
+                <h3>EFFICIENT DISTRIBUTION SERVICES</h3>
+                <p>Explore our seamless distribution solutions tailored to your business. With precision and speed, we handle your products, ensuring they reach their destinations swiftly and securely. From local deliveries to nationwide distribution, we optimize your supply chain for maximum efficiency.</p>
+            </div>
+            <div class="distribution-service-wrapper">
+                <div class="card-icon">
+              <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92.35 122.88" style="enable-background:new 0 0 92.35 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path id="dist-service" class="st0" d="M46.18,0.01c2.17-0.09,3.88,0.66,5.61,1.76c2.19,1.39,4.66,4.14,7.71,5.88c4.29,2.45,12.23-0.93,16.29,5.11 c2.37,3.52,2.48,6.28,2.66,9.01c0.19,2.94,0.71,5.65,3.72,9.63c4.99,6.6,6.03,10.99,3.46,15.56c-1.75,3.12-5.44,4.85-6.29,6.83 c-1.82,4.2,0.19,7.37-2.29,12.27c-1.73,3.4-4.39,5.64-7.94,6.78c-2.99,0.96-5.99-0.43-8.39,0.58c-4.21,1.77-7.31,5.88-10.66,6.92 c-1.29,0.4-2.58,0.6-3.87,0.59c-1.29,0.01-2.58-0.19-3.87-0.59c-3.35-1.04-6.45-5.15-10.66-6.92c-2.4-1.01-5.4,0.39-8.39-0.58 c-3.55-1.14-6.21-3.38-7.94-6.78c-2.49-4.9-0.48-8.07-2.29-12.27c-0.85-1.98-4.54-3.71-6.29-6.83C4.16,42.39,5.2,38,10.19,31.41 c3.01-3.98,3.53-6.69,3.72-9.63c0.18-2.73,0.29-5.49,2.66-9.01c4.07-6.04,12.01-2.66,16.29-5.11c3.05-1.74,5.52-4.49,7.71-5.88 C42.29,0.67,44.01-0.09,46.18,0.01L46.18,0.01z M46.18,25.97l4.46,10.9l11.75,0.87l-8.99,7.61l2.8,11.44l-10.02-6.2l-10.02,6.2 l2.8-11.44l-8.99-7.61l11.75-0.87L46.18,25.97L46.18,25.97z M88.96,113.07L77.41,111l-5.73,10.26c-4.16,5.15-6.8-3.32-7.96-6.27 L52.57,93.96c2.57-0.89,5.67-3.46,8.85-6.35c6.35,0.13,12.27-0.97,16.62-6.51l12.81,24.75l1.11,2.38 C92.84,111.32,92.38,113.36,88.96,113.07L88.96,113.07z M3.39,113.07L14.95,111l5.73,10.26c4.16,5.15,6.8-3.32,7.96-6.27 l11.15-21.03c-2.57-0.89-5.67-3.46-8.85-6.35c-6.35,0.13-12.27-0.97-16.62-6.51L1.5,105.85l-1.11,2.38 C-0.49,111.32-0.03,113.36,3.39,113.07L3.39,113.07z M46.06,16.1c13.8,0,24.99,11.19,24.99,24.99c0,13.8-11.19,24.99-24.99,24.99 c-13.8,0-24.99-11.19-24.99-24.99C21.08,27.29,32.26,16.1,46.06,16.1L46.06,16.1z"/></g></svg>                </div>
+                <H3>RELIABLE PRODUCT DISTRIBUTION</H3>
+                <p>Count on us for reliable product distribution services. Our dedicated team and advanced logistics ensure your products are delivered on time. Whether it's small parcels or large shipments, we guarantee safe and efficient distribution solutions for your business.</p>
+            </div>
+            <div class="distribution-service-wrapper">
+                <div class="card-icon">
+<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 109.57 122.88"><title>distribute</title><path id="product" d="M51.83,29.77V21.89a11.12,11.12,0,0,1-5-18.62h0a11.09,11.09,0,0,1,15.71,0h0A11.13,11.13,0,0,1,57.17,22v7.47L57,29.39a6.51,6.51,0,0,0-.92-.18,10.81,10.81,0,0,0-1.24,0h-.14a6.82,6.82,0,0,0-1,.07c-.23,0-.51.1-.82.18a6.71,6.71,0,0,0-1,.31l-.08,0Zm-18,17.51L54.1,35.93l.25-.1a2.05,2.05,0,0,1,.65-.1,2.1,2.1,0,0,1,.56.07A1.09,1.09,0,0,1,56,36L76.06,47.61a2.08,2.08,0,0,1,1.11,1,2.65,2.65,0,0,1,.3,1.3V73.32a2.26,2.26,0,0,1-1.19,1.85L56.13,86.88a2,2,0,0,1-.46.18,2.15,2.15,0,0,1-1.23,0,2.18,2.18,0,0,1-.56-.25l-20.61-12A2.26,2.26,0,0,1,32.1,73V49.46a2.26,2.26,0,0,1,1.7-2.18Zm19,33.81V62.77L36.6,53.12V71.56l16.21,9.53ZM73,53.4,57.31,62.78V81.11L73,71.86V53.4ZM55,40.47,39.69,49.61l15.38,9.26,14.92-9L55,40.47ZM57.32,101a11.18,11.18,0,0,1,5.21,2.94h0a11.09,11.09,0,0,1,0,15.71h0a11.09,11.09,0,0,1-15.71,0h0a11.09,11.09,0,0,1,0-15.71h0A11.09,11.09,0,0,1,52,101v-7.8a6.6,6.6,0,0,0,.66.23v0l.38.1a4.9,4.9,0,0,0,.78.16,8.48,8.48,0,0,0,1.26.11,8.37,8.37,0,0,0,1.26-.11,6.44,6.44,0,0,0,1-.22V101Zm1.44,6.7a5.82,5.82,0,0,0-8.18,0h0a5.83,5.83,0,0,0,0,8.19h0a5.82,5.82,0,0,0,8.18,0h0a5.81,5.81,0,0,0,0-8.19ZM58.76,7a5.82,5.82,0,0,0-8.18,0h0a5.83,5.83,0,0,0,0,8.19,5.83,5.83,0,0,0,4.1,1.69A5.79,5.79,0,0,0,58.77,7ZM98.44,75.44a11.12,11.12,0,0,1,7.86,3.26h0a11.09,11.09,0,0,1,0,15.71h0A11.12,11.12,0,0,1,87.79,83.34l-6.51-3.76a8.36,8.36,0,0,0,1.45-1.8,9.85,9.85,0,0,0,.51-1,6.45,6.45,0,0,0,.39-1,8.06,8.06,0,0,0,.23-.91l6.65,3.84.06-.07a11.13,11.13,0,0,1,7.87-3.26Zm4.09,7a5.79,5.79,0,1,0,0,8.21h0a5.81,5.81,0,0,0,0-8.19Zm-91.4-7a11.1,11.1,0,0,1,7.74,3.14l6.84-4,.06.24.16.61a7.15,7.15,0,0,0,.47,1.17,4.79,4.79,0,0,0,.4.78,8.91,8.91,0,0,0,1.44,1.79l.06.06-6.6,3.81A11.12,11.12,0,0,1,3.27,94.43h0a11.13,11.13,0,0,1,7.87-19Zm4.09,7a5.81,5.81,0,0,0-8.19,0,5.82,5.82,0,0,0,0,8.19H7a5.83,5.83,0,0,0,8.19,0,5.82,5.82,0,0,0,0-8.2ZM98.44,25.13a11.12,11.12,0,0,1,7.87,19h0a11.12,11.12,0,0,1-15.73,0L90.43,44,83.82,47.8a8.6,8.6,0,0,0-.92-2.41,8.87,8.87,0,0,0-1.72-2.22l6.58-3.8a11.07,11.07,0,0,1,2.81-11h0a11.08,11.08,0,0,1,7.86-3.26Zm4.09,7a5.81,5.81,0,0,0-8.18,0h0a5.79,5.79,0,0,0,8.19,8.2h0a5.79,5.79,0,0,0,0-8.19Zm-91.4-7A11,11,0,0,1,19,28.39h0a11,11,0,0,1,3.26,7.85,11.32,11.32,0,0,1-.31,2.62l6.58,4a7.31,7.31,0,0,0-.83.84,8.46,8.46,0,0,0-1.33,2.06l-.27.6c-.12.35-.23.69-.32,1l-6.29-3.82a6.15,6.15,0,0,1-.49.53A11.12,11.12,0,1,1,3.26,28.4h0a11.08,11.08,0,0,1,7.86-3.26Zm4.09,7a5.83,5.83,0,0,0-8.19,0H7a5.83,5.83,0,0,0,0,8.19,5.79,5.79,0,0,0,9.89-4.1,5.77,5.77,0,0,0-1.68-4.09Z"/></svg>                </div>
+                <h3>STREAMLINED SUPPLY CHAIN</h3>
+                <p>We simplify your supply chain with our streamlined distribution services. Through careful planning and execution, we optimize routes and reduce transit times, saving you costs and ensuring your products reach customers seamlessly. Experience the difference with our professional distribution expertise.</p>
+            </div>
+
+        </div>
+      
+
+    </div>
+   
+    <?php
+    include("form.php");
+    ?>
+     <div id="area" class="area">
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+    <?php
+     include("map.php");
+    include("footer.html");
+    ?>
+</body>
