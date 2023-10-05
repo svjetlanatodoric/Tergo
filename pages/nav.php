d9615ec753ebf482ffbe622a7658c45f82ebb853
Svjetlana <svjetlanatodoric03@gmail.com>
2023-10-05 12:20:45 +0200

    Added dropdown on services link and added structure of hamburger menu, later styled for smaller screens

--- pages/nav.php
+++ pages/nav.php
@@ -1,15 +1,40 @@
-<div class="white-bg">
+<body>
+  <div class="white-bg">
+  <a href="index.php">
     <div class="header-logo-container">
         <img src="../img/Tergo_logo.png" alt="">
     </div>
+    </a>
     <div class="links">
-        <a href="index.php">HOME</a>
+      <a href="index.php">HOME</a>
+
+      <div class="dropdown">
         <a href="index.php#services">SERVICES</a>
-        <a href="index.php#about-us">ABOUT US</a>
-        <a href="#contact-us">CONTACT</a>
-        <!-- <div class="search-icon">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </div> -->
+        <div class="dropdown-content">
+          <a href="warehousing.php">Warehousing</a>
+          <a href="road-transport.php">Road transport</a>
+          <a href="distribution.php">Distribution</a>
+          <a href="container-handling.php">Container handling</a>
+        </div>
+      </div>
+
+      <a href="index.php#about-us">ABOUT US</a>
+      <a href="#contact-us">CONTACT</a>
+
+    </div>
+    <!-- MENU FOR SMALL DEVICES -->
+    <div id="burger-menu">
+      <span></span>
     </div>
 
-</div>
+    <div id="menu">
+      <ul>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="index.php#services">SERVICES</a></li>
+        <li><a href="index.php#about-us">ABOUT US</a></li>
+        <li><a href="index.php#contact-us">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+  <script src="../js/nav.js"></script>
+</body>
