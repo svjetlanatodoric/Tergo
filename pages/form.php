ad8175e2e9664070cd4bde6ed7f4d7a6ed9126e0
Svjetlana <svjetlanatodoric03@gmail.com>
2023-10-05 12:21:36 +0200

    Changed way of sending email.Before EmailJS waas used, now implemented PHP code.

--- pages/form.php
+++ pages/form.php
@@ -1,52 +1,52 @@
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<div class="form-section" id="contact-us">
-   
-    <div class="form">
-    <div class="form-header">
-            <p class="form-title" id="contact-anchor">FEEL FREE TO CONTACT US!</p>
+</head>
+
+<body>
+
+    <div class="form-section" id="contact-us">
+
+        <div class="form">
+            <div class="form-header">
+                <p class="form-title" id="contact-anchor">FEEL FREE TO CONTACT US!</p>
+            </div>
+            <form id="contactForm" method="POST" action="<?php echo filter_var($_SERVER['PHP_SELF'].'#contact-us'); ?>">
+                <div class="name-container">
+                    <input type="text" class="name" name="name" id="name" placeholder="Your name: " asterisk="*" required>
+                </div>
+
+                <div class="email-container">
+                    <input type="email" id="email" name="email" class="email" placeholder="Your e-mail:" required>
+
+                </div>
+                <div class="textarea-container">
+                    <textarea id="msg" class="msg" name="msg" placeholder="Enter message:"></textarea>
+                </div>
+                <input type="submit" id="submit" name="submit" value="SEND!">
+            </form>
+            <?php
+
+            if (isset($_POST['submit'])) {
+                ini_set('SMTP', 'tergo.biz');
+                ini_set('smtp_port', 25);
+                $name = htmlspecialchars($_POST['name']);
+                $sender = htmlspecialchars($_POST['email']);
+                $to = "info@tergo.biz";
+                $headers  = "MIME-Version: 1.0" . "\r\n";
+                $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+                $headers  .= "From: " . $sender . "\r\n";
+                $subject = "Message from " . $name;
+                $message = $_POST["msg"];
+
+                if (@mail($to, $subject, $message, $headers)) {
+                    echo "<p style='font-family:roboto; margin-bottom:10px;'>Message sent successfully!</p>";
+                    }
+                
+                }
+
+            ?>
         </div>
-        <form id="contactForm" method="POST" action="">
-            <div class="name-container">
-                <input type="text" class="name" name="name" id="name" placeholder="Your name: " asterisk="*" required>
-            </div>
-
-            <div class="email-container">
-                <input type="email" id="email" name="email" class="email" placeholder="Your e-mail:" required>
-
-            </div>
-            <div class="textarea-container">
-                <textarea id="msg" class="msg" name="msg" placeholder="Enter message:"></textarea>
-            </div>
-            <input type="submit" value="SEND!">
-            <script>
-                const form = document.getElementById("contactForm"); // Assuming you have a form with this ID
-
-                form.addEventListener("submit", function (event) {
-                    event.preventDefault();
-                    emailjs.init("X76dSkwqPXfGf2t8S");
-
-                    const name = document.getElementById("name").value;
-                    const email = document.getElementById("email").value;
-                    const message = document.getElementById("msg").value;
-
-                    const templateParams = {
-                        name: name,
-                        email: email,
-                        message: message
-                    };
-
-                    // Call emailjs.send with templateParams to send the email
-                    emailjs.send("service_ue7zkpx", "template_izs0m5k", templateParams)
-                        .then(function (response) {
-                            alert("Email sent successfully!");
-                            console.log(email)
-                        }, function (error) {
-                            alert("Email sending failed. Please try again later.");
-                            console.error("Email.js error:", error);
-                        });
-                });
-            </script>
-        </form>
     </div>
-    </div>
-
+    <script src="../js/form.js"></script>
+</body>
